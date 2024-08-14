6f856fc31ae509ecbbed20bc95b0b5683a86cfed
Xavier DMello <xaviermdmello@gmail.com>
2024-08-13 18:10:13 -0700

    remove ugly text

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -38,20 +38,7 @@ function App() {
 
           <ConnectKitButton mode="light" />
           <ViewSelection />
-          <p className="read-the-docs">This is for demo purposes only.</p>
-          <p className="read-the-docs">
-            Please read our{" "}
-            <a
-              href="https://github.com/risc0/demos/blob/main/bonsai-pay/README.md"
-              target="_blank"
-              rel="noreferrer"
-            >
-              docs
-            </a>{" "}
-            for more information.
-          </p>
         </div>
-        <Footer />
         <ToastContainer />
       </ConnectKitProvider>
     </WagmiConfig>
@@ -97,70 +84,3 @@ function ViewSelection() {
 }
 
 export default App;
-
-function Footer() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalContent, setModalContent] = useState<{
-    title: string;
-    content: ReactNode;
-  }>({ title: "", content: "" });
-
-  const openModal = (title: string, content: ReactNode) => {
-    setModalContent({ title, content });
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  return (
-    <footer className="footer">
-      <Modal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        title={modalContent.title}
-      >
-        {modalContent.content}
-      </Modal>
-      <a href="https://www.risczero.com/news/bonsai-pay">About</a>
-      <a
-        href="https://github.com/risc0/demos/tree/main/bonsai-pay"
-        className="footer-link"
-      >
-        Github
-      </a>
-
-      <button
-        onClick={() =>
-          openModal(
-            "Terms of Service",
-            <iframe
-              className="tos-content"
-              src="./BonsaiPayTermsofService2023.11.07.html"
-              title="Terms of Service"
-            />
-          )
-        }
-        className="footer-button"
-      >
-        Terms of Service
-      </button>
-      <button
-        onClick={() =>
-          openModal(
-            "Privacy Policy",
-            <iframe
-              className="privacy-content"
-              src="./RISCZeroBonsaiWebsitePrivacyPolicy2023.11.07.html"
-              title="Privacy Policy"
-            />
-          )
-        }
-        className="footer-button"
-      >
-        Privacy Policy
-      </button>
-    </footer>
-  );
-}
